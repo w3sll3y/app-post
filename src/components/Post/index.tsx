9b4ce42d5cdf05f153757f97d4e1b8a54d2f5aea
Wesley Fernandes <fernandess.weslley@gmail.com>
2024-03-08 23:53:27 -0300

    adjust like methods

--- src/components/Post/index.tsx
+++ src/components/Post/index.tsx
@@ -1,6 +1,9 @@
-import { View, Text } from "react-native";
 import * as Styled from './styles';
 import GetUser from "./components/GetUser";
+import { useEffect, useState } from "react";
+import axios from 'axios';
+import Toast from 'react-native-toast-message';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 type Post = {
   id: number,
@@ -23,11 +26,77 @@ export default function Post({
   typeMachine,
   likes_count
 }: Post) {
+  const [token, setToken] = useState('');
+  const [liked, setLiked] = useState(false);
+  const [countLiked, setCounLikes] = useState<Number>(Number(likes_count));
+
+  async function getUser() {
+    const storedUsername = await AsyncStorage.getItem('token');
+    setToken(JSON.parse(storedUsername)?.token);
+  }
+
+  async function handleUpdateLikes(numberLikes: number) {
+    try {
+      const apiUrl = `http://192.168.0.26/api_post/posts/like/${id}`;
+      const headers = {
+        'authorization': `Bearer ${token}`,
+        'Content-Type': 'application/json',
+      };
+
+      const data = {
+        likes_count: String(numberLikes),
+      }
+
+      console.log('aqui', String(numberLikes))
+      const response = await axios.put(apiUrl, data, { headers });
+      console.log('resp', response);
+      if (response?.data?.tipo === "sucesso") {
+        // showToast();
+      }
+    } catch (err) {
+      return Toast.show({
+        type: 'error',
+        text1: 'Erro ao curtir',
+        text2: 'Tente novamente!'
+      });
+    }
+  }
+
+  function handleLikeUnliked() {
+    let totalLikes = Number(countLiked);
+    if (liked === false) {
+      console.log(liked)
+      totalLikes++;
+      setCounLikes(totalLikes);
+      handleUpdateLikes(totalLikes);
+    }
+    if (liked === true) {
+      console.log(liked)
+      totalLikes = totalLikes - 1;
+      setCounLikes(totalLikes);
+      handleUpdateLikes(totalLikes);
+    }
+    setLiked(!liked)
+  }
+
+  useEffect(() => {
+    getUser();
+  }, []);
+
   return (
     <Styled.Container>
       <Styled.Title>{title}</Styled.Title>
       <Styled.Description>{description}</Styled.Description>
       <GetUser id={idAuthor} />
+      <Styled.LikedContainer>
+        <Styled.LikeTitle>likes: {countLiked}</Styled.LikeTitle>
+        <Styled.Liked
+          name="like2"
+          size={24}
+          liked={liked}
+          onPress={handleLikeUnliked}
+        />
+      </Styled.LikedContainer>
     </Styled.Container>
   );
 }
--- src/components/Post/styles.ts
+++ src/components/Post/styles.ts
@@ -1,3 +1,4 @@
+import { AntDesign } from '@expo/vector-icons';
 import styled from "styled-components/native";
 
 export const Container = styled.View`
@@ -23,4 +24,26 @@ export const CreatedBy = styled.Text`
   font-family: ${({ theme }) => theme.FONT_FAMILY.REGULAR};
   margin-top: 10px;
   font-style: italic;
+`;
+
+export const Liked = styled(AntDesign) <{ liked?: boolean }>`
+  font-family: ${({ theme }) => theme.FONT_FAMILY.REGULAR};
+  color: ${(props) => (props.liked ? '#00B37E' : '#202024')};
+
+  margin-top: 10px;
+  font-style: italic;
+`;
+
+export const LikeTitle = styled.Text`
+  font-family: ${({ theme }) => theme.FONT_FAMILY.REGULAR};
+  margin-top: 10px;
+  font-style: italic;
+`;
+
+export const LikedContainer = styled.View`
+  margin-top: 10px;
+  width: 100%;
+  align-items: center;
+  justify-content: space-between;
+  flex-direction: row;
 `;
